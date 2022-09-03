00b699fc2beced1620c06e9e90b750062ed09741
Micheal Earl <Micheal-Earl@users.noreply.github.com>
2022-09-03 08:35:26 -0700

    Fixed init

--- main.go
+++ main.go
@@ -1,33 +1,12 @@
 package main
 
-import "fmt"
-
-func testFunction(c chan string) {
-	c <- "Hello Channel"
-	c <- "String 2"
-	c <- "String 3"
-	c <- "String 4"
-	c <- "String 5"
-	c <- "String 6"
-	c <- "String 7"
-	c <- "String 8"
-	c <- "String 9"
-}
+import "mbc.mikesprogram.com/blockchain"
 
 func main() {
-	c := make(chan string, 2)
+	chain := blockchain.NewBlockChain()
+	chain.AddBlock([]byte("Hello"))
+	chain.AddBlock([]byte("Hello1"))
+	chain.AddBlock([]byte("Hello2"))
 
-	go testFunction(c)
-
-	for {
-		fmt.Println(<-c)
-		if len(c) == 0 {
-			break
-		}
-	}
+	chain.Print()
 }
-
-//chain := blockchain.NewBlockChain()
-//chain.AddBlock([]byte("Hello"))
-//chain.AddBlock([]byte("Hello1"))
-//chain.AddBlock([]byte("Hello2"))
